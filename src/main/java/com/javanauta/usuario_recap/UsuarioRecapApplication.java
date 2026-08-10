92a87f20e6b52496978f052b55a1f63dab79e01b
Gabriel de Oliveira Cardoso <108769102+cardosogoc@users.noreply.github.com>
2026-08-10 15:01:50 -0300

    commit inicial

---
+++ src/main/java/com/javanauta/usuario_recap/UsuarioRecapApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.usuario_recap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioRecapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/usuario_recap/UsuarioRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.usuario_recap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioRecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
